0c98f7a7df0a937b7e9499671c8287ecb60bf3a8
missfionajean <missfionajean@gmail.com>
2024-10-21 09:59:44 -0400

    set up working directory with necessary files

---
+++ app.js
@@ -0,0 +1,30 @@
+// connects app.js to data.js
+const pokemon = require('./data.js')
+
+const game = {
+    party: [],
+    gyms: [
+      { location: "Pewter City", completed: false, difficulty: 1 },
+      { location: "Cerulean City", completed: false, difficulty: 2 },
+      { location: "Vermilion City", completed: false, difficulty: 3 },
+      { location: "Celadon City", completed: false, difficulty: 4 },
+      { location: "Fuchsia City", completed: false, difficulty: 5 },
+      { location: "Saffron City", completed: false, difficulty: 6 },
+      { location: "Cinnabar Island", completed: false, difficulty: 7 },
+      { location: "Viridian City", completed: false, difficulty: 8 },
+    ],
+    items: [
+      { name: "potion", quantity: 4 },
+      { name: "pokeball", quantity: 8 },
+      { name: "rare candy", quantity: 99 },
+    ],
+}
+
+
+/* EXERCISE 1
+// displays all objects in pokemon array, null length lets it go over 100
+console.dir(pokemon, { maxArrayLength: null })
+
+// logs a single pokemon, in this case #59 (remember that it's base 0)
+console.log(pokemon[58])
+*/
